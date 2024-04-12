c6500ef002d915e2dea6ef8d6c89a0ce88aac0af
francis-tg <franc9885@gmail.com>
2024-04-12 11:36:45 +0000

    add dropdown and login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiChevronDown, FiSearch, } from "react-icons/fi"
 import {IoClose} from "react-icons/io5"
+import Login from '../pages/Login'
 function Navbar() {
+    const [showLogin,setLoginShow] = useState(false)
     function showSearchBar() {
         document.querySelector(".search-overlay").classList.contains("hidden")&&
         document.querySelector(".search-overlay").classList.replace("hidden","flex")
@@ -10,6 +12,9 @@ function Navbar() {
     function closeSearchBar() {
         document.querySelector(".search-overlay").classList.contains("flex")&&
         document.querySelector(".search-overlay").classList.replace("flex","hidden")
+    }
+    function onShowLogin() {
+        setLoginShow(!showLogin)
     }
   return (
     <div className='relative'>
@@ -23,25 +28,59 @@ function Navbar() {
                         <span>Homepage</span>
                           <FiChevronDown />
                           <ul className='hover-dropdown-list'>
-                              <li>
-                                  <Link to={"#"}>
+                              <li className=''>
+                                  <Link to={"#"} className=' '>
                                       Page1
                                   </Link>
                               </li>
-                               <li>
-                                  <Link to={"#"}>
+                               <li className=''>
+                                  <Link to={"#"} className=''>
                                       Page2
                                   </Link>
                               </li>
                           </ul>
                     </Link>
-                    <Link to={""} className="flex items-center">
+                    <Link to={""} className="flex items-center hover-dropdown">
                         <span>Receipe page</span>
-                        <FiChevronDown/>
+                          <FiChevronDown />
+                           <ul className='hover-dropdown-list'>
+                              <li className=''>
+                                  <Link to={"#"} className=' '>
+                                      Receipe 1
+                                  </Link>
+                              </li>
+                               <li className=''>
+                                  <Link to={"#"} className=''>
+                                      Receipe 2
+                                  </Link>
+                              </li>
+                              <li className=''>
+                                  <Link to={"#"} className=''>
+                                      Receipe 3
+                                  </Link>
+                              </li>
+                          </ul>
                     </Link>
-                    <Link to={""} className="flex items-center">
+                    <Link to={""} className="flex items-center hover-dropdown">
                         <span>Pages</span>
-                        <FiChevronDown/>
+                          <FiChevronDown />
+                           <ul className='hover-dropdown-list'>
+                              <li className=''>
+                                  <Link to={"#"} className=' '>
+                                      Receipe 1
+                                  </Link>
+                              </li>
+                               <li className=''>
+                                  <Link to={"#"} className=''>
+                                      Receipe 2
+                                  </Link>
+                              </li>
+                              <li className=''>
+                                  <Link to={"#"} className=''>
+                                      Receipe 3
+                                  </Link>
+                              </li>
+                          </ul>
                     </Link>
                     <Link to={""} >
                        Buy
@@ -49,20 +88,22 @@ function Navbar() {
                 </div>
             </div>
             <div className='flex items-center gap-4'>
-                <FiSearch size={30} onClick={showSearchBar} />
-                <div className='w-10 h-10 rounded-full'>
+                <FiSearch size={30} onClick={showSearchBar} className='hover:text-orange-500 duration-500 transition-all hover:cursor-pointer' />
+               {/*  <div className='w-10 h-10 rounded-full'>
                     <img src={require("../img/menu10.png")} className='w-10 h-10 rounded-full' alt="" />
-                </div>
+                </div> */}
+                <button className='bg-orange-500 px-8 py-2 rounded text-white' onClick={onShowLogin}>Login</button>
             </div>
           </nav>
           
-          <div className='absolute top-20 w-full justify-center items-center z-50 h-screen duration-500 transition-all search-overlay hidden'>
+          <div className='absolute top-0 w-full justify-center items-center z-50 h-screen duration-500 transition-all search-overlay hidden'>
               <div className='relative'>
                   <input type="search" placeholder='Search receipe her...' className='p-3 border  w-[600px] outline-none rounded' />
                   <button className='border-none absolute right-0 bg-white rounded text-3xl p-2 text-orange-500'><FiSearch/></button>
               </div>
               <IoClose className='text-3xl absolute top-0 right-2 cursor-pointer ' onClick={closeSearchBar}/> 
           </div>
+          <Login showLogin={showLogin} onCloseLogin={onShowLogin}/>
     </div>
   )
 }
